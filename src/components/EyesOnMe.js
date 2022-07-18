3d69444eb639983fcc6d50d77c006bffb2e2d599
Bryan Bosire <bryanbosire@gmail.com>
2022-07-18 08:54:58 +0300

    commit tree b59a41c3394735db54190ce4e94c2c9dd9784892

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    function handleFocus(){
+        console.log("Good!");
+    }
+
+    function handleBlur(){
+        console.log("Hey! Eyes on me!");
+    }
+    return(
+        <form onFocus={handleFocus} onBlur={handleBlur} >
+            <button>Eyes on me</button>
+        </form>
+    )
+}
+
+export default EyesOnMe;
